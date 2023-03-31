24002536f473bf70001b3a5a1da951ae880ad3ac
MoserD3 <qt3@mail.ru>
2023-04-01 00:08:37 +0300

    first commit

---
+++ LowerFiles/Main.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace LowerFiles
+{
+    internal class Program
+    {
+        static private DateTime Date;
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Date = DateTime.Now;
+                Console.WriteLine("Current Date: " + Date.ToString());
+
+                foreach (var dir in Directory.GetDirectories(args[0], "*", SearchOption.AllDirectories))
+                {
+                    LowerFiles(dir);
+                }
+                LowerFiles(args[0]);
+                Console.WriteLine("Done");
+            }
+            else
+            {
+                Console.WriteLine("0 Arguments");
+            }
+
+            Console.ReadKey();
+        }
+
+        static void LowerFiles(string dir)
+        {
+            foreach (var file in Directory.GetFiles(dir))
+            {
+                File.SetCreationTime(file, Date);
+                File.SetLastAccessTime(file, Date);
+                File.SetLastWriteTime(file, Date);
+                Console.WriteLine("Rename File: " + file);
+                File.Move(file, file.ToLowerInvariant());
+            }
+        }
+    }
+}
---
+++ LowerFiles/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LowerFiles")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LowerFiles")]
+[assembly: AssemblyCopyright("Copyright © 2023 by Moser")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("bb4e7daa-7414-42c4-8900-9d37ee805313")]
+[assembly: AssemblyVersion("23.3.31.0")]
+[assembly: AssemblyFileVersion("23.3.31.0")]
